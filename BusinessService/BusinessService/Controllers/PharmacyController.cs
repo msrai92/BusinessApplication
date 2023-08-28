16a65a01e4d1e992963cfd54277ae7d48b242b4f
msrai92 <m.singh.rai15@gmail.com>
2023-08-28 09:27:16 -0500

    Remove default controller; Create pharmacy controller and routes

---
+++ BusinessService/BusinessService/Controllers/PharmacyController.cs
@@ -0,0 +1,35 @@
+﻿using BusinessService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusinessService.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class PharmacyController : ControllerBase
+    {
+        private static List<Pharmacy> _pharmacyList = new List<Pharmacy>
+        {
+            new Pharmacy { Id = 1, Name = "Test Pharmacy 1"},
+            new Pharmacy { Id = 2, Name = "Test Pharmacy 2"},
+            new Pharmacy { Id = 3, Name = "Test Pharmacy 3"},
+        };
+
+        [HttpGet("GetPharmacyById/{id}")]
+        public ActionResult<Pharmacy> Get(int id)
+        {
+            return Ok(_pharmacyList.FirstOrDefault(p => p.Id == id));
+        }
+
+        [HttpGet("GetPharmacies")]
+        public ActionResult<List<Pharmacy>> GetAll()
+        {
+            return Ok(_pharmacyList);
+        }
+
+        [HttpPut("UpdatePharmacy")]
+        public ActionResult<Pharmacy> UpdatePharmacy(Pharmacy pharmacy)
+        {
+            return Ok(pharmacy);
+        }
+    }
+}
---
+++ BusinessService/BusinessService/Models/Pharmacy.cs
@@ -0,0 +1,16 @@
+﻿namespace BusinessService.Models
+{
+    public class Pharmacy
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string Zip { get; set; } = string.Empty;
+        public long NumberOfFilledPrescriptions { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+    }
+}
